96122c4accc629127db2b5b3853f9cbb93fc7c20
Dil Sombayeva <dilyasom@gmail.com>
2019-08-17 11:28:43 -0700

    add server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const helmet = require('helmet');
+const ProjectRouter = require('./project/project-router');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use('/api/projects', ProjectRouter);
+
+server.get('/', (req, res) => {
+ res.send('<h2>Home Page!</h2>');
+});
+
+module.exports = server;
