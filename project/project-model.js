2862ce68e79642c454081ad4ae1a15ea14a34091
Dil Sombayeva <dilyasom@gmail.com>
2019-08-17 15:00:09 -0700

    add findById helper f

--- project/project-model.js
+++ project/project-model.js
@@ -3,10 +3,14 @@ const db = require('../data/db-config');
 module.exports = {
  find,
  add,
+ findById,
 };
 
 function find() {
  return db('projects');
+}
+function findById(id) {
+ return db('projects').where({ id });
 }
 
 function add(info) {
